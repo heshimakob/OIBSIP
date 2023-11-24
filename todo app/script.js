619b94526a588876fcd3844afede3441029ac0c7
heshima kob <heshimakob@gmail.com>
2023-11-24 07:15:50 +0100

    edit function and delete function on file script

--- todo app/script.js
+++ todo app/script.js
@@ -1,4 +1,10 @@
 let taskListArray=[];
+let isLocalDataPresent= localStorage.getItem("todoTaskList");
+if(isLocalDataPresent !== null){
+    taskListArray=JSON.parse(isLocalDataPresent);
+    renderTaskList();
+
+}
 function saveTask(){
     debugger;
     let taskName= document.getElementById("txtItem").value;
@@ -7,8 +13,12 @@ function saveTask(){
         taskName:taskName
     };
     taskListArray.push(todoObject);
-    renderTaskList()
-
+    localStorage.setItem("todoTaskList",JSON.stringify(taskListArray));
+    renderTaskList();
+}
+function removeAll(){
+    taskListArray.splice(0);
+    renderTaskList();
 }
 function renderTaskList(){
     debugger;
@@ -22,9 +32,40 @@ function renderTaskList(){
         myLabel.appendChild(myPara);
         dynamicli.appendChild(myLabel);
 
+
+        let myDiv =document.createElement("div");
+        myDiv.classList.add("settings");
+        let editIcon= document.createElement("i")
+        editIcon.classList.add("fa ");
+        editIcon.classList.add("fa fa-pencile-square");
+        editIcon.addEventListener("click", editTask);
+        editIcon.taskId = taskListArray[index].taskId;
+
+        let deleteIcon= document.createElement("i");
+        deleteIcon.classList.add("fa");
+        deleteIcon.classList.add("fa fa-trash");
+        deleteIcon.addEventListener("click", deleteTask);
+        deleteIcon.taskId = taskListArray[index].taskId;
+
+        myDiv.appendChild(editIcon);
+        myDiv.appendChild(deleteIcon);
+        dynamicli.appendChild(myDiv);
+
         document.getElementById("myTaskList").appendChild(dynamicli);
 
     }
+}
+function deleteTask(event){
+    debugger;
+    let index= taskListArray.findIndex(m=>m.taskId == event.target.taskId);
+    taskListArray.splice(index,1);
+    localStorage.setItem("todoTaskList",JSON.stringify(taskListArray));
+    renderTaskList();
 
-  
+}
+function editTask(event){
+    debugger;
+    let obj= taskListArray.find(m=>m.taskId == event.target.taskId);
+    document.getElementById("txtItem").value= obj.taskName;
+
 }
