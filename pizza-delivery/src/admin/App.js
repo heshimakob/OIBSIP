8251a3c1115e6d1b1840fcc54b7496669ec68e4a
heshima kob <heshimakob@gmail.com>
2023-11-30 08:27:45 +0100

    folder admin

---
+++ pizza-delivery/src/admin/App.js
@@ -0,0 +1,22 @@
+import Home from './screens/Home';
+import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
+import Login from '../screens/Login'
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/order" element={<Order/>}/> */}
+          <Route exact path="/" element={<Login/>}/>
+        </Routes>
+      </div>
+
+    </Router>
+   
+  );
+}
+
+export default App;
