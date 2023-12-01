8e6533e7a670482210f9b01c1204d165c1fdce4d
heshima kob <heshimakob@gmail.com>
2023-12-01 14:50:21 +0100

    pop up

--- pizza-delivery/src/components/Carroussel.js
+++ pizza-delivery/src/components/Carroussel.js
@@ -1,45 +1,79 @@
-import React,{useState} from 'react'
-import { Card,Row,Col, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Card, Row, Col, Button, Modal } from "react-bootstrap";
 
+export default function Carroussel({ pizza }) {
+  const [show, setShow] = useState(false);
 
-export default function Carroussel({pizza}) {
-  const [ varient,setVarient]=useState("small")
-  const [quantity,setQuantity]=useState(1)
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const [varient, setVarient] = useState("small");
+  const [quantity, setQuantity] = useState(1);
   return (
-    <Card style={{ width: '18rem' ,marginTop:"10px"}}>
-    <Card.Img variant="top" src={pizza.image}  />
-    <Card.Body>
-      <Card.Title>{pizza.name}</Card.Title>
-      <Card.Text>
-        <Row>
-          <Col md={6}>
-            <h6>Varient</h6>
-            <select value={varient} onChange={e=>setVarient(e.target.value)}> 
-              {pizza.varients.map((varient)=>(
-                <option  >{varient}</option>
+    <>
+      <Card style={{ width: "18rem", marginTop: "10px" }}>
+        <Card.Img variant="top" src={pizza.image} onClick={handleShow} style={{cursor:"pointer"}} />
+        <Card.Body>
+          <Card.Title>{pizza.name}</Card.Title>
+          <Card.Text>
+            <Row>
+              <Col md={6}>
+                <h6>Varient</h6>
+                <select
+                  value={varient}
+                  onChange={(e) => setVarient(e.target.value)}
+                >
+                  {pizza.varients.map((varient) => (
+                    <option>{varient}</option>
+                  ))}
+                </select>
+              </Col>
+              <Col md={6}>
+                <h6>Quantity</h6>
+                <select
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                >
+                  {[...Array(10).keys()].map((v, i) => (
+                    <option key={i} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  ))}
+                </select>
+              </Col>
+            </Row>
+          </Card.Text>
+          <Row>
+            <Col md={6}> Price:${pizza.prices[0][varient] * quantity} </Col>
+            <Col>
+              <Button className="bg-primary text-white">Add to store</Button>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+          <Card.Img variant="top" src={pizza.image}  />
 
-              ))}
-            </select>
-          </Col>
-          <Col md={6}>
-          <h6>Quantity</h6>
-          <select value={quantity}  onChange={(e)=>setQuantity(e.target.value)}>
-            {[...Array(10).keys()].map((v, i ) =>(
-        <option key={i} value={i + 1}
-       >{i+1}</option>
-       ))}
-            </select>
-          </Col>
-        </Row>
-      </Card.Text>
-     <Row>
-      <Col md={6}> Price:${pizza.prices[0][varient]*quantity} </Col>
-                <Col>
-                <Button className='bg-primary text-white'>Add to store</Button>
-                </Col>
-     </Row>
-    </Card.Body>
-  </Card>
-    
-  )
+          </div>
+          <div>
+            <h4>description:</h4>
+            <h5>description:{pizza.description}</h5>
+          </div>
+          </Modal.Body>
+       
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
