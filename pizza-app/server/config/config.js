832f3055028c056a7a5dbdb6c95bffdbe9dbe606
heshima kob <heshimakob@gmail.com>
2023-12-03 10:43:28 +0100

    data import to mongodb

--- pizza-app/server/config/config.js
+++ pizza-app/server/config/config.js
@@ -1,32 +1,32 @@
 
 const mongoose = require("mongoose");
+require('dotenv').config();
 require("colors");
+// const URI= "mongodb+srv://heshimakob:Kobinali%4025111950@cluster0.7s5moqn.mongodb.net/pizza-shop";
+// const connectDB=async()=>{
 
-const connectDB=async()=>{
-
-  try {
-    const URI= "mongodb+srv://heshimakob:Kobinali%4025111950@cluster0.7s5moqn.mongodb.net/pizza-shop";
-    const conn=await mongoose.connect(URI);
-    console.log("server connecter".bgCyan.white)
-    
-  } catch (error) {
-    console.log("echec de connexion".bgRed.white);
-    process.exit(0)
-    
-  }
-}
-
-// const connectDB = async () => {
 //   try {
-//     const url = process.env.MONGO_URI;
-//     const conn = await mongoose.connect(url, {
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true,
-//       useCreateIndex: true
-//     })
-//     console.log(`Database connected  ${conn.connection.host}`.bgCyan.white);
+//     await mongoose.connect(URI);
+//     console.log("server connecter".bgCyan.white)
+    
 //   } catch (error) {
-//     console.log(`error: ${error.message}`.bgRed.white);
+//     console.log("echec de connexion".bgRed.white);
+//     process.exit(0)
+    
 //   }
-// };
+// }
+
+const connectDB = async () => {
+  try {
+    const url = process.env.MONGO_URI;
+    const conn = await mongoose.connect(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      // useCreateIndex: true
+    })
+    console.log(`Database connected  ${conn.connection.host}`.bgCyan.white);
+  } catch (error) {
+    console.log(`error: ${error.message}`.bgRed.white);
+  }
+};
 module.exports = connectDB;
