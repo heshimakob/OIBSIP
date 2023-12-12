0be13d3927688d2749620d5a111c1c100f9905b6
heshima kob <heshimakob@gmail.com>
2023-12-12 12:58:36 +0100

    form add pizza admin page

--- pizza-app/pizza-delivery/src/components/Admin/AddNewPizza.js
+++ pizza-app/pizza-delivery/src/components/Admin/AddNewPizza.js
@@ -13,6 +13,22 @@ const AddNewPizza = () => {
     const[mediumprice,setsmediumPrice]=useState('')
     const[largeprice,setlargePrice]=useState('')
     const[description,setdecription]=useState('')
+    const[image,setimage]=useState('')
+    const[category,setcategory]=useState('')
+    const submitForm =(e)=>{
+        e.preventDefault();
+        const pizza ={
+            name,image,description,category,
+            prices:{
+                small:smallprice,
+                medium:mediumprice,
+                large:largeprice
+            }
+           
+
+        }
+        console.log(pizza)
+    }
 
     // const [validated, setValidated] = useState(false);
 
@@ -28,76 +44,111 @@ const AddNewPizza = () => {
   return (
     <>
   
-  <Form noValidate >
+  <Form  onSubmit={submitForm} >
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
+          <Form.Label> name</Form.Label>
           <Form.Control
             required
             type="text"
-            placeholder="First name"
-            defaultValue="Mark"
+            placeholder="  name"
+            Value={name}
+            onChange={e=> setname(e.target.value)}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
+          <Form.Label>small</Form.Label>
           <Form.Control
-            required
-            type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
+           required
+           type="text"
+           placeholder="small"
+           Value={smallprice}
+           onChange={e=> setsmallPrice(e.target.value)}
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+        {/* <Form.Group as={Col} md="4" controlId="validationCustomUsername">
           <Form.Label>Username</Form.Label>
           <InputGroup hasValidation>
             <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
             <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
+               required
+               type="text"
+               placeholder="medium"
+               Value={mediumprice}
+               onChange={e=> setsmediumPrice(e.target.value)}
             />
             <Form.Control.Feedback type="invalid">
               Please choose a username.
             </Form.Control.Feedback>
           </InputGroup>
-        </Form.Group>
+        </Form.Group> */}
       </Row>
       <Row className="mb-3">
         <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
+          <Form.Label>Medium</Form.Label>
+          <Form.Control required
+           type="text"
+           placeholder="small"
+           value={mediumprice}
+           onChange={(e)=> setsmediumPrice(e.target.value)} />
           <Form.Control.Feedback type="invalid">
             Please provide a valid city.
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
+          <Form.Label>Large</Form.Label>
+          <Form.Control  type="text"
+           placeholder="small"
+           value={largeprice}
+           onChange={(e)=> setlargePrice(e.target.value)} />
           <Form.Control.Feedback type="invalid">
             Please provide a valid state.
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
+          <Form.Label>image</Form.Label>
+          <Form.Control   placeholder="small"
+           value={image}
+           onChange={(e)=> setimage(e.target.value)} />
           <Form.Control.Feedback type="invalid">
             Please provide a valid zip.
           </Form.Control.Feedback>
         </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label> description</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="  description"
+            value={description}
+            onChange={(e)=> setdecription(e.target.value)}
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label> category</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder=" Enter catgorie"
+            value={category}
+            onChange={(e)=> setcategory(e.target.value)}
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
       </Row>
-      <Form.Group className="mb-3">
+      {/* <Form.Group className="mb-3">
         <Form.Check
           required
           label="Agree to terms and conditions"
           feedback="You must agree before submitting."
           feedbackType="invalid"
         />
-      </Form.Group>
-      <Button type="submit">Submit form</Button>
+      </Form.Group> */}
+      <Button variant='primary' type='submit'>Add new pizza</Button>
     </Form>
     </>
   )
